6ad217d3e74eae3d102b02cb5ae2d6bddcf11a1e
Yehor <egorkorsun7@gmail.com>
2024-06-11 14:05:12 +0200

    model for filter

---
+++ BookStore/Models/BookFilterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class BookFilterViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Style { get; set; }
+        public string Theme { get; set; }
+        public string PublishingHouse { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than or equal to {1}")]
+        public int? MinPages { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than or equal to {1}")]
+        public int? MaxPages { get; set; }
+
+        [Range(
+            1,
+            double.MaxValue,
+            ErrorMessage = "Please enter a value greater than or equal to {1}"
+        )]
+        public int? MinCost { get; set; }
+
+        [Range(
+            1,
+            double.MaxValue,
+            ErrorMessage = "Please enter a value greater than or equal to {1}"
+        )]
+        public int? MaxCost { get; set; }
+
+        public DateTime? PublishedAt { get; set; }
+    }
+}
